0d0737c3f8fdf55cd906f733d5c847a438e0d845
Sun Tan <sutan@redhat.com>
2020-10-19 07:50:11 +0000

    update kubernetes client namspace usage

--- src/main/java/org/eclipse/che/kubernetes/KubernetesClientService.java
+++ src/main/java/org/eclipse/che/kubernetes/KubernetesClientService.java
@@ -11,7 +11,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.fabric8.kubernetes.api.model.Pod;
@@ -43,8 +42,26 @@ public class KubernetesClientService {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("{namespace}")
-    public List<Pod> pods(@PathParam("namespace") String namespace) {
-        return kubernetesClient.pods().inNamespace(namespace).list().getItems();
+    @Path("pods")
+    public List<Pod> pods() {
+        return kubernetesClient.pods().list().getItems();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("namespace/{namespace}")
+    public String setNamespace(@PathParam("namespace") String namespace) {
+        kubernetesClient.getConfiguration().setNamespace(namespace);
+        return kubernetesClient.getNamespace();
+    }
+
+    
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("namespace")
+    public String getNamespace() {
+        return kubernetesClient.getNamespace();
     }
 }
